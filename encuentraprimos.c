09951f84dd715db7f22cc612d97649a702a50982
Adrian <adrian.rubiboy11@gmail.com>
2022-12-26 00:25:50 +0100

    Añade el esqueleto

--- encuentraprimos.c
+++ encuentraprimos.c
@@ -68,5 +68,74 @@ void alarmHandler(int signo)
 
 int main(int argc, char **argv)
 {
+	key_t key;
+
+	T_MSG_BUFFER message;
+
+	int pid, msgid, mypid, parentpid, pidservidor;
+
+	int numhijos = 2; // Cambiar lectura por consola
+	if ((pid = fork()) == 0) // Creacion del servidor (SERVER)
+	{
+		pid = getpid();
+		pidservidor = pid;
+
+		if (( key = ftok( "/tmp", 'C' )) == -1 )
+		{
+			perror("Fallo al pedir ftok");
+			exit( 1 );
+		}
+		printf("Server: System V IPC key = %u\n", key);
+
+		if (( msgid = msgget( key, IPC_CREAT | 0666 )) == -1 )
+		{
+			perror( "Fallo al crear la cola de mensajes" );
+			exit( 2 );
+		}
+		printf("Server: Message queue id = %u\n", msgid );
+
+		for (int i=0; i<numhijos; i++)
+		{
+			if (pid > 0)
+			{
+				if ((pid = fork()) == 0)
+				{
+					parentpid = getppid();
+					mypid = getpid();
+				}
+			}
+		}
+
+		if (mypid != pidservidor)
+		{
+			message.msg_type = COD_ESTOY_AQUI;
+			sprintf(message.msg_text, "%d", mypid);
+			msgsnd(msgid, &message, sizeof(message), IPC_NOWAIT);
+
+			sleep(60);
+			exit(0);
+		}
+
+		else
+		{
+			for (int j=0; j<numhijos; j++)
+			{
+				msgrcv(msgid, &message, sizeof(message), 0, 0);
+				sscanf(message.msg_text, "%d", &pid);
+				printf("\nMe ha enviado un mensaje el hijo %d\n", pid);
+			}
+
+			sleep(60);
+			msgctl(msgid, IPC_RMID, NULL);
+		}
+	}
+
+	else
+	{
+		alarm(INTERVALO_TIMER);
+		signal(SIGALRM, alarmHandler);
+		for (;;) sleep(1);
+	}
+
 	return 0;
 }
